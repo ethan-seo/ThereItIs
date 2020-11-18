de4755b5e39f1855b2393a6439043ad498b1737e
Ethan Seo <sung.min.seo.428@gmail.com>
2020-11-17 23:12:34 -0800

    Added Django Admin, updated nav bars on all 4 page htmls

--- ThereItIs_app/admin.py
+++ ThereItIs_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Item, Transaction
+
+admin.site.register(User)
+admin.site.register(Item)
+admin.site.register(Transaction)

633c3a2128d5d8cc8d9defe0d55441320e4a1a0b
Jonathanpaniagua <jonathan.r.paniagua@gmail.com>
2020-11-22 13:57:45 -0800

    updated js to fix search fxn on dashboard table 2

--- ThereItIs_app/static/thereitis.js
+++ ThereItIs_app/static/thereitis.js
@@ -1,7 +1,15 @@
-// DATA TABLE
+// FOR ALL DATA TABLES
 
 $(document).ready(function() {
     $('#example').DataTable( {
+        "order": [[ 3, "desc" ]]
+    } );
+} );
+
+// FOR TABLE 2 ON DASHBOARD
+
+$(document).ready(function() {
+    $('#dash_table2').DataTable( {
         "order": [[ 3, "desc" ]]
     } );
 } );

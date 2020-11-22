be36ebfc5af9bfe0fe6bdbe98088af7bffe5e84c
Jonathanpaniagua <jonathan.r.paniagua@gmail.com>
2020-11-22 13:49:06 -0800

    made table items links, added footer to index for extension, updated datatable js, added expiring icon in navbar

--- ThereItIs_app/static/thereitis.js
+++ ThereItIs_app/static/thereitis.js
@@ -1,17 +1,8 @@
 // DATA TABLE
 
-$(document).ready(function () {
-    $('#example').DataTable({
-        columnDefs: [{
-            orderable: false,
-            className: 'select-checkbox',
-            targets: 0
-        }],
-        select: {
-            style: 'os',
-            selector: 'td:first-child'
-        },
-        order: [[1, 'asc']]
-    });
-});
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "order": [[ 3, "desc" ]]
+    } );
+} );
 

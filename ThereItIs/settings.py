8071b3d4df8d1ef001a24feb6f662ba5875a075e
Ethan Seo <sung.min.seo.428@gmail.com>
2020-11-23 22:16:52 -0800

    Updated transactions functionality and various html files

--- ThereItIs/settings.py
+++ ThereItIs/settings.py
@@ -122,8 +122,8 @@ STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# MEDIA_ROOT =  os.path.join(BASE_DIR, 'ThereItIs_app/static/media') 
-# MEDIA_URL = '/ThereItIs_app/static/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/ThereItIs/static/media/'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR,'ThereItIs_app/static/media'),
